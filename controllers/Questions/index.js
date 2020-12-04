9f5e4a9010e221292ae46b9b6f6bcff55fe8651a
Christopher <Christopherliang7@gmail.com>
2020-12-04 18:49:26 -0500

    Created file structure for backend In process of building model queries Created Routers

--- controllers/Questions/index.js
+++ controllers/Questions/index.js
@@ -1,3 +1,30 @@
-// var {your exports} = require('../../models/Questions');
+const { 
+  modelsGetQuestions, 
+  modelsGetAnswers, 
+  modelsPostQuestion, 
+  modelsPostAnswer, 
+  modelsHelpfulQuestion, 
+  modelsReportQuestion, 
+  modelsHelpfulAnswer, 
+  modelsReportAnswer } = require('../../models/Questions');
 
-// insert your controller functions
+module.exports = {
+  // Questions
+  getQuestions: (req, res) => {
+  },
+  postQuestion: (req, res) => {
+  },
+  helpfulQuestion: (req, res) => {
+  },
+  reportQuestion: (req, res) => {
+  },
+  // Answers
+  getAnswers: (req, res) => {
+  },
+  postAnswer: (req, res) => {
+  },
+  helpfulAnswer: (req, res) => {
+  },
+  reportAnswer: (req, res) => {
+  }
+};
