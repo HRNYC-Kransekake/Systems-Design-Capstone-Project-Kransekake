c51ef2e2e30b2d2694c4f8e9b3a772c1ee4b363b
Kai Gong <kaigong@Kais-MacBook-Pro.local>
2020-12-07 21:54:51 -0500

    finalized all the requests for reviews

--- models/Reviews/reviews-model.js
+++ models/Reviews/reviews-model.js
@@ -25,18 +25,18 @@ module.exports = {
       }
     });
   },
-  create: function (params, callback) {
+  createReview: function (params, callback) {
+    let date = new Date().toISOString().slice(0, 10);
     // create a message for a user id based on the given username
-    var queryStr =
-      'insert into reviews(product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness) \
-                    value (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-    db.query(queryStr, params, function (err, results) {
+    var queryStr = `insert into reviews (product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness)
+       values (?, ?, ${JSON.stringify(date)}, ?, ?, ?, 0, ?, ?, null, 0)`;
+    reviewDb.query(queryStr, params, function (err, results) {
       callback(err, results);
     });
   }, // a function which can be used to insert a message into the database
 
   // a function that will extract all the metadatas from database
-  GetAllMeta: function (params, callback) {
+  getAllMeta: function (params, callback) {
     var queryStr = `select (JSON_ARRAYAGG(rating))AS ratings,
       sum(recommend) AS recommended,
       (SELECT json_objectagg((select characteristics.name from characteristics where characteristics.id = characteristic_reviews.characteristic_id) ,JSON_object('id', characteristic_reviews.id, 'value', characteristic_reviews.value))
@@ -53,6 +53,30 @@ module.exports = {
         }
         console.log(results);
         callback(err, results);
+      }
+    });
+  },
+  markHelpful: function (review_id, callback) {
+    const queryStr = `UPDATE reviews\
+     SET helpfulness = helpfulness + 1\
+     WHERE id = ${review_id};`;
+    reviewDb.query(queryStr, (error, result) => {
+      if (error) {
+        console.log(error);
+      } else {
+        callback(null, result);
+      }
+    });
+  },
+  markReport: function (review_id, callback) {
+    const queryStr = `UPDATE reviews\
+     SET reported = 1\
+     WHERE id = ${review_id};`;
+    reviewDb.query(queryStr, (error, result) => {
+      if (error) {
+        console.log('Error with reportQuestions query: ', error);
+      } else {
+        callback(null, result);
       }
     });
   },
